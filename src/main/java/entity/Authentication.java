3e95800695e42da64b959dc14d93d7eb248ed580
naoya.shikatani <chao_gadiop_kun@yahoo.co.jp>
2023-04-11 22:30:47 +0900

    Lesson35課題

---
+++ src/main/java/entity/Authentication.java
@@ -0,0 +1,43 @@
+package entity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Authentication")
+public class Authentication {
+
+    public static enum role{
+        一般, 管理者
+    }
+
+    /** 社員番号 **/
+    @Id
+    @Column(length = 20, nullable = false)
+    private String code;
+
+
+    /** パスワード **/
+    @Column(length = 255)
+    private String password;
+
+    /** 権限 **/
+    @Column(length = 10, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private String role;
+
+    /** 従業員のテーブルのID **/
+    @Column(name = "table_id")
+    @JoinColumn(name = "table_id", referencedColumnName = "id")
+    private Integer employee_id;
+
+}
---
+++ src/main/java/entity/Employee.java
@@ -0,0 +1,35 @@
+package entity;
+
+import java.sql.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Employee")
+public class Employee {
+
+    /** 主キー。自動生成 */
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    /** 名前。20桁。null不許可 */
+    @Column(length = 20, nullable = false)
+    private String name;
+
+    /** 削除フラグ **/
+    private Integer delete_flag;
+    /** 登録日時 **/
+    private Date created_at;
+    /** 更新日時 **/
+    private Date updated_at;
+
+}
